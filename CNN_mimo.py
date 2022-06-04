548582d5a4fda4f88e7afb73d96181943f92d2d7
alarbi-anas <72145816+alarbi-anas@users.noreply.github.com>
2022-06-04 07:01:01 +0200

    Update CNN_mimo.py

--- CNN_mimo.py
+++ CNN_mimo.py
@@ -47,8 +47,8 @@ if __name__ == '__main__':
     n_pilots = 2
 
     use_other_estimators = False
-    use_genie_estimators = False
-    use_cnn_estimators = True
+    use_genie_estimators = True
+    use_cnn_estimators = False
     plot_axis = "snr"
 
     transforms = 'fft'
